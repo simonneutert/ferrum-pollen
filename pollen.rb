5f8310bd2b81e27204396bba9b97a08baa80ea9f
Simon Neutert <simonneutert@users.noreply.github.com>
2024-09-17 14:54:40 +0200

    housekeeping

--- pollen.rb
+++ pollen.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'ferrum'
+require 'bundler/setup'
+Bundler.require
 
 browser = Ferrum::Browser.new(browser_options: { 'no-sandbox': nil })
 
@@ -23,13 +24,12 @@ sleep 1
   browser.keyboard.type([:down])
   sleep 0.5
 end
-date_for_image =  Time.now.strftime("%Y-%m-%d")
+date_for_image = Time.now.strftime('%Y-%m-%d')
 browser.screenshot(path: "pollen-#{date_for_image}.png")
 browser.quit
 
 # add correct image
 `sed -i 's#.*img src=.*#  <img src="pollen-#{date_for_image}.png">#' index.html`
 
-timestamp = Time.now.strftime("%Y-%m-%d %H:%M")
+timestamp = Time.now.strftime('%Y-%m-%d %H:%M')
 `sed -i 's#.*span.*#  <span>Update #{timestamp}</span>#' index.html`
-
